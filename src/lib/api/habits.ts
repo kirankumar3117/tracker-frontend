f945fcabd4a67a4f62819ad2ff6e2a9a631bbf0f
kiran kumar <kirankumar@kirans-MacBook-Air.local>
2026-03-04 10:21:32 +0530

    public url changes

--- src/lib/api/habits.ts
+++ src/lib/api/habits.ts
@@ -1,6 +1,6 @@
 import { Habit, HabitLog } from "@/types/habit";
 
-const API_BASE_URL = "http://localhost:8000/api/v1";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
 
 const getAuthHeaders = () => {
   const token = typeof window !== "undefined" ? localStorage.getItem("tracker-token") : null;
